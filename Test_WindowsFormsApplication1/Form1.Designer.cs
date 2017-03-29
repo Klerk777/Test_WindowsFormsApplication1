a1cb00f7cc45e4f1633f5613779977e4eeb15882
admin-pc11 <admin-pc11@PC11>
2017-03-29 09:34:24 +0300

    Добавьте файлы проекта.

---
+++ Test_WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Test_WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.submit_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox1.Location = new System.Drawing.Point(12, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Enter email";
+            this.textBox1.UseWaitCursor = true;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic);
+            this.textBox2.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox2.Location = new System.Drawing.Point(12, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Enter group or department";
+            this.textBox2.UseWaitCursor = true;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fill data";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(4, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(113, 19);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "access recovery";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(4, 66);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(130, 19);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "educational issues";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(4, 100);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(115, 19);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "technical issues";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton5.Location = new System.Drawing.Point(4, 66);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(44, 19);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "CIT";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton6.Location = new System.Drawing.Point(4, 32);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(79, 19);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "support@";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(354, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Select details";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(252, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(139, 120);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sort of problem";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(403, 59);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(115, 120);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Support@ or CIT";
+            // 
+            // submit_button
+            // 
+            this.submit_button.AutoEllipsis = true;
+            this.submit_button.BackColor = System.Drawing.Color.Gray;
+            this.submit_button.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold);
+            this.submit_button.Location = new System.Drawing.Point(140, 210);
+            this.submit_button.Name = "submit_button";
+            this.submit_button.Size = new System.Drawing.Size(110, 34);
+            this.submit_button.TabIndex = 13;
+            this.submit_button.Text = "Submit";
+            this.submit_button.UseVisualStyleBackColor = false;
+            this.submit_button.Click += new System.EventHandler(this.submit_button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-2, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "neik.nlu.edu.ua";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(176, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "oldlib.nlu.edu.ua";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(357, 275);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "send mail";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 152);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            this.label3.Location = new System.Drawing.Point(12, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 14);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Setup date:";
+            // 
+            // button4
+            // 
+            this.button4.AutoEllipsis = true;
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(256, 210);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 34);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Clean";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(529, 296);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.submit_button);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Accounting appeals";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button submit_button;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Test_WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Test_WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private Excel.Application excelapp;
+        private Excel.Workbooks excelappworkbooks;
+        private Excel.Workbook excelappworkbook;
+        private Excel.Window excelWindow;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Enter email")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+                textBox1.Font = new Font(textBox1.Font, FontStyle.Regular);
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Enter email";
+                textBox1.ForeColor = Color.DarkGray;
+                textBox1.Font = new Font(textBox1.Font, FontStyle.Italic);
+
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Enter group or department")
+            {
+                textBox2.Text = "";
+                textBox2.ForeColor = Color.Black;
+                textBox2.Font = new Font(textBox2.Font, FontStyle.Regular);
+            }
+        }
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Enter group or department";
+                textBox2.ForeColor = Color.DarkGray;
+                textBox2.Font = new Font(textBox2.Font, FontStyle.Italic);
+            }
+        }
+
+
+        // Description to Event submit_button clik
+        private void submit_button_Click(object sender, EventArgs e)
+        {
+            string mail = textBox1.Text, group = textBox2.Text;
+            string radiobut1 = "", radiobut2 = "";
+            //var buttons1 = groupBox1.Controls.OfType<RadioButton>().Where(n => n.Checked);
+            //foreach (RadioButton rb in buttons1)
+            //{
+            //    textBox3.Text = (rb.Text);
+            //}
+
+            //var buttons2 = groupBox2.Controls.OfType<RadioButton>().Where(n => n.Checked);
+            //foreach (RadioButton rb in buttons2)
+            //{
+            //    textBox4.Text = (rb.Text);
+            //}
+
+            //groupBox1.Count
+            //foreach (RadioButton rb in groupBox1)
+            //   for (int i = 0; i < groupBox1.Count; i++)
+            //{
+            //    if (radioButton[i].Checked)
+            //    {
+            //        MessegeBox.Show("Нашлась красавица");
+            //    }
+            //textBox3.Text = (problem + "     " + group);
+
+
+
+            //List<RadioButton> buttons = new List<RadioButton>();
+            //foreach (GroupBox gb in Controls.OfType<GroupBox>())
+            //{
+            //    foreach (RadioButton rb in gb.Controls.OfType<RadioButton>())
+            //    {
+            //        if (rb.Checked)
+            //        {
+            //            buttons.Add(rb);
+            //            break;
+            //        }
+            //    }
+            //}
+            //foreach (RadioButton rb in buttons)
+            //{
+            //    MessageBox.Show(rb.Text);
+            //}
+
+            //MessageBox.Show(dateTimePicker1.Text);
+
+            // List<RadioButton> buttons = new List<RadioButton>();
+            foreach (RadioButton rb in groupBox1.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                {
+                    radiobut1 = rb.Text;
+                    break;
+                }
+            }
+
+            foreach (RadioButton rb in groupBox2.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                {
+                    radiobut2 = rb.Text;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                MessageBox.Show((new string[5] { dateTimePicker1.Text, textBox1.Text, textBox2.Text, radiobut1, radiobut2 })[i]);
+            }
+
+            //excelapp = new Excel.Application();
+            //excelapp.Visible = true;
+            //excelapp.SheetsInNewWorkbook = 1;
+            //excelapp.Workbooks.Add(Type.Missing);
+            excelapp.Cells[1, 1] = dateTimePicker1.Text;
+            excelapp.Cells[1, 2] = textBox1.Text;
+            excelapp.Cells[1, 3] = textBox2.Text;
+            excelapp.Cells[1, 4] = radiobut2;
+            excelapp.Cells[1, 5] = radiobut1;
+            excelapp.DisplayAlerts = true;
+            //excelappworkbooks = excelapp.Workbooks;
+            //excelappworkbook = excelappworkbooks[1];
+            //excelappworkbook.Saved = true;            
+            //excelapp.Quit();
+        }
+
+
+        // Opening links on Event click buttons
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://neik.nlu.edu.ua");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://oldlib.nlu.edu.ua");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("It's dont work yet, Motherfucka", "Attention!");
+        }
+
+        // Refresh form on click "Clear" button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Enter email";
+            textBox1.ForeColor = Color.DarkGray;
+            textBox1.Font = new Font(textBox1.Font, FontStyle.Italic);
+
+            textBox2.Text = "Enter group or department";
+            textBox2.ForeColor = Color.DarkGray;
+            textBox2.Font = new Font(textBox2.Font, FontStyle.Italic);
+
+            foreach (RadioButton rb in groupBox1.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked == true)
+                { rb.Checked = false; }
+            }
+            foreach (RadioButton rb in groupBox2.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked == true)
+                { rb.Checked = false; }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //excelapp.DefaultSaveFormat = Excel.XlFileFormat.xlSYLK;
+            //excelapp.Quit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string mySheet = @"‪E:\Book1.xlsx";
+            var excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            Excel.Workbooks books = excelApp.Workbooks;
+            try
+            {
+                Excel.Workbook sheet = books.Open(mySheet);
+            }
+            catch (COMException ex)
+            {
+                int error = (ex.ErrorCode);                
+                MessageBox.Show(error.ToString());
+            }
+
+
+
+                //         excelapp.Workbooks.Open(@"C:\Book1.xlsx",
+                //Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                //Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                //Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                //Type.Missing, Type.Missing);
+            }
+        }
+}
